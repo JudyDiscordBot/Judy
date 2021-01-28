e454f79f54e88058491f2ddd8c1a713c40f4f8f4
MrAndre2020 <ninfetahot123@gmail.com>
2021-01-27 22:14:54 -0300

    Colocando catch no comando de pesquisar nas docs do discord.js

--- commands/Membros/discordCmd.js
+++ commands/Membros/discordCmd.js
@@ -5,11 +5,16 @@ module.exports.run = async(client, message, args) => {
      args = args.splice(0).join(" ");
              if (!args[0]) return message.quote("Voce precisa escrever algo para pesquisar nas docs")
      const qParams = new URLSearchParams({ q: args });
+
+     try {
      axios.get(URL + `&${qParams.toString()}`)
       .then(response => {
             message.quote({ embed: response.data })
-      })          
-      message.quote(`Caso o bot não responda nada, quer dizer que não foi encontrado.`)
+      })
+} catch {
+      return message.quote(`Não encontrei nada, desculpe`)
+}
+
 };
 
 exports.help = {
