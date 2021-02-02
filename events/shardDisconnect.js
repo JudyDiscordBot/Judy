c0c0b3e2c3afb5b5d1070974d8ce4cc38ea0e1e5
MrAndre2020 <ninfetahot123@gmail.com>
2021-02-02 14:50:22 -0300

    Removido sistema de logs, provavelmente ele vai voltar, mas em outra instancia de hospedagem.

--- events/shardDisconnect.js
+++ events/shardDisconnect.js
@@ -1,13 +1,19 @@
 const { client, config } = require("../index");
-const { MessageEmbed, WebhookClient } = require("discord.js")
+const {WebhookClient } = require("discord.js")
 
 client.on('shardDisconnect', (event, shardID) => {
     let hook = new WebhookClient('799668672104759396', config.webhook.shard)
-    let embed = new MessageEmbed()
-    embed.setColor('#FFC4E7')
-    embed.setAuthor(`Tombo Triste`)
-    embed.setDescription(`Shard \`${shardID}\` se desconectou`)
-    embed.setFooter(`${client.user.tag} - (${client.user.id})`)
-    embed.setTimestamp()
-    hook.send(embed)
+    hook.send({
+        "embed": 
+          {
+            "description": `Shard \`${shardID}\` se desconectou`,
+            "color": 16762087,
+            "author": {
+              "name": "Tombo Triste"
+            },
+            "footer": {
+              "text": `${client.user.tag} - (${client.user.id})`
+            }
+          }
+      })
 })
--- events/shardReconnect.js
+++ events/shardReconnect.js
@@ -1,14 +1,19 @@
 const { client, config } = require("../index");
-const { MessageEmbed, WebhookClient } = require("discord.js")
+const {WebhookClient} = require("discord.js")
 
 
 client.on('shardReconnecting', id => {
     let hook = new WebhookClient(`799668672104759396`, config.webhook.shard)
-    let embed = new MessageEmbed()
-    embed.setAuthor(`Voltei animada !!!`)
-    embed.setColor('#FFC4E7')
-    embed.setDescription(`Shard \`${id}\` se reconectou`)
-    embed.setFooter(`${client.user.tag} - (${client.user.id})`)
-    embed.setTimestamp()
-    hook.send(embed)
+    hook.send({
+        "embed": {
+            "description": `Shard \`${id}\` se reconectou`,
+            "color": 16762087,
+            "author": {
+              "name": "Voltei animada !!!"
+            },
+            "footer": {
+              "text": `${client.user.tag} - (${client.user.id})`
+            }
+          }
+      })
 })
