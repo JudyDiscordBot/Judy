806fd4cd694dcc213654eb9128c850ec1c143ea9
MrAndre2020 <ninfetahot123@gmail.com>
2021-02-02 23:35:28 -0300

    Mudei denovo, cade a logica disso nnsei

--- events/HandlerEvent.js
+++ events/HandlerEvent.js
@@ -1,25 +1,21 @@
 const {client, config} = require("../index")
 const {MessageEmbed, WebhookClient} = require("discord.js-light");
 const comando = new WebhookClient('799683143937818634', config.webhook.log)
-const bldb = require("../mongodb/blacklist.js")
-const p = require("../mongodb/prefix");
-const user = require('../mongodb/user.js');
+//const user = require('../mongodb/user.js');
 client.cooldown = new Set()
 const cooldowns = {}
 const ms = require('ms');
-const { truncate } = require("fs/promises");
 
 client.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type == 'dm') return;
 
-p.findOne({name: "prefix", preid: message.guild.id}).then(async res => {
-let prefix = res ? res.prefix : config.bot.prefix;
+let prefix = config.bot.prefix;
     if (!message.content.toLowerCase().startsWith(prefix.toLowerCase())) return;
     if(message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)){
       return  message.quote(`**Olá eu sou a Judy, meu prefixo *nesse servidor* é \`${prefix}\`, use \`${prefix}ajuda\` para ver meus comandos.**`)}
 
-  user.findOne({id:message.author.id}, (err, db) => {
+  /* user.findOne({id:message.author.id}, (err, db) => {
     if(db) return;
     if(!db) {
       var date = new Date();
@@ -32,19 +28,12 @@ let prefix = res ? res.prefix : config.bot.prefix;
       message.quote(`<a:danacomigo:760151015583514655> Olá ${message.author}, como você não tinha uma conta criada em meu sistema eu acabei de criar uma pra você, espero que você goste de mim e que aproveite meus sistemas !!!! Hoje é um dia incrível, irei registrar esse exato momento que nos conhecemos em meu banco de dados para podermos comemorar ano que vem !!! <a:super_happy:801534215467827271>`)
     }
   })
-
+*/
    var argscm = message.content.substring(prefix.length).split(" ");
    let cmdcm = argscm.shift().toLowerCase();
    let commandcm = client.commands.get(cmdcm) || client.commands.get(client.aliases.get(cmdcm))
 
-   if(commandcm) {
-
-if(client.cooldown.has(message.author.id)) return
-client.cooldown.add(message.author.id);
-setTimeout(()=>{
-client.cooldown.delete(message.author.id)
-}, 100000)
-
+if(commandcm) {
 if(!cooldowns[message.author.id]) cooldowns[message.author.id] = {
   lastCmd: null
 }
@@ -85,26 +74,5 @@ try {
             .addField('Dados do executor:', `ID: ${message.author.id}\nUsername: ${message.author.username}\nUser Tag: ${message.author.tag}`)
             .addField('Mensagem:', `${message.content}`)
             .addField('Dados do servidor:', `Membros: ${message.guild.memberCount}\nNome: ${message.guild.name}\nID do server: ${message.guild.id}`);
-
-            const commandlogs = {
-              fields: [
-                {
-                  name: 'Dados do executor:',
-                  value: `ID: ${message.author.id}\nUsername: ${message.author.username}\nUser Tag: ${message.author.tag}`,
-                  inline: true,
-                },
-                {
-                  name: 'Mensagem:',
-                  value: `${message.content}`,
-                  inline: true,
-                },
-                {
-                  name: 'Dados do servidor:',
-                  value: `Membros: ${message.guild.memberCount}\nNome: ${message.guild.name}\nID do server: ${message.guild.id}`,
-                  inline: true,
-                }
-              ]
-            }
             comando.send(embeddiretor);
-  })
 });
