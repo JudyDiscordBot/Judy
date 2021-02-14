f6b9ca860abad0e2d509561baf1072e992991432
MrAndre2020 <ninfetahot123@gmail.com>
2021-02-14 00:40:59 -0300

    Removido profile command

--- commands/Servidor/resetCmd.js
+++ commands/Servidor/resetCmd.js
@@ -1,8 +1,6 @@
-const Discord = require('discord.js-light')
-const mongoose = require('mongoose')
 const messagelog = require('../../mongodb/messagelog.js')
 const serverlog = require('../../mongodb/serverlog')
-
+const ch = require('../../mongodb/channelcmd.js')
 module.exports.run = async (bot, message, args, prefix) => {
   if(!message.member.hasPermission('ADMINISTRATOR')) {
   return message.quote('❌ Você precisa de permissão de ADMINISTRADOR para ultilizar este comando')
@@ -10,10 +8,8 @@ module.exports.run = async (bot, message, args, prefix) => {
 
   if(!args[0]) {
   
-  message.quote(`**Resete algum sistema do Banco de Dados.\n\nUse esse comando para remover os canais de logs !!!\n\ \`${prefix}resetar messagelog\` - Resete o canal de logs de mensagens do servidor\n\`${prefix}resetar serverlog\` - Resete o canal de logs de atualizações do servidor**`)
+  message.quote(`**Resete algum sistema do Banco de Dados.\n\nUse esse comando para remover os canais de logs ou comandos !!!\n\ \`${prefix}resetar messagelog\` - Resete o canal de logs de mensagens do servidor\n\`${prefix}resetar serverlog\` - Resete o canal de logs de atualizações do servidor\n\`${prefix}resetar channelcommand\` - Resete o canal de comandos do servidor**`)
   } else
-if(message.author.bot || message.channel.type === 'dm') {return;}
-        let messageinfocontent = message.content.toLowerCase()
     switch(args[0]) {
       case 'messagelog':
         messagelog.deleteOne({ GuildID: message.guild.id }, (err) => console.log(err))
@@ -22,6 +18,10 @@ if(message.author.bot || message.channel.type === 'dm') {return;}
       case 'serverlog':
       serverlog.deleteOne({ GuildID: message.guild.id }, (err) => console.log(err))
       message.quote(`O canal de atualizações do servidor foi resetado com sucesso.`)
+      break;
+      case 'channelcommand':
+      ch.deleteOne({guild:message.guild.id }, (err) => console.log(err))
+      message.quote(`O canal de comandos foi resetado com sucesso.`)
     }
 }
 exports.help = {
